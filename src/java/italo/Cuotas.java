ff6df6d24c67b583040177b16b2dfa7398f04ba4
marcelo <marcelo@marcelo-franco>
2014-11-02 11:06:57 -0300

    modelo actualizado

--- src/java/italo/Cuotas.java
+++ src/java/italo/Cuotas.java
@@ -8,6 +8,7 @@ package italo;
 
 import java.io.Serializable;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,6 +18,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
